35b128698dd603a3c84754548cf2dc87ef56d14f
xts <xts@qq.com>
2024-06-18 15:51:04 +0800

    first commit

---
+++ src/main/java/com/itclass/git/GitDemo.java
@@ -0,0 +1,11 @@
+package com.itclass.git;
+
+/**
+ * @author: 徐泰森
+ * @create: 2024-06-18 15:44
+ **/
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello xts~~~");
+    }
+}

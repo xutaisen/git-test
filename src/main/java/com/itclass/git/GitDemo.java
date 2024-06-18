a6e3dbb3bd3cebdec052b4d32fa12864d97e9e7c
xts <xts@qq.com>
2024-06-18 15:59:58 +0800

    second commit

--- src/main/java/com/itclass/git/GitDemo.java
+++ src/main/java/com/itclass/git/GitDemo.java
@@ -1,11 +1,20 @@
 package com.itclass.git;
 
+import java.util.Scanner;
+
 /**
  * @author: 徐泰森
  * @create: 2024-06-18 15:44
  **/
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("hello xts~~~");
+        System.out.println("请输入一个大于零的数：");
+        Scanner scanner = new Scanner(System.in);
+       int i = scanner.nextInt();
+        if (i>1){
+            System.out.println("傻狗");
+        }else {
+            System.out.println("真聪明~~");
+        }
     }
 }
